8485113a5334853c47d50e7d7b452d7447da877c
vivanova288-boop <vivanova288@gmail.com>
2025-12-19 18:08:11 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-
-const repoName = 'Courcefigma'
 
 export default defineConfig({
-  base: `/${repoName}/`,
-  plugins: [
-    react(),
-    tailwindcss(), // ✅ ВОТ ОНА — КЛЮЧЕВАЯ СТРОКА
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/Courcefigma/',
+  plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
