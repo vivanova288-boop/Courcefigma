f68733d31b2ac504afc687eaead87b0b7222d9ea
vivanova288-boop <vivanova288@gmail.com>
2025-12-19 19:30:27 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app/App'
-import './styles/index.css'   // ← ВОТ ЭТО ОБЯЗАТЕЛЬНО
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
