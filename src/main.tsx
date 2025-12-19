8028c040be88b725200e940cbb4107d09b8c7cd2
vivanova288-boop <vivanova288@gmail.com>
2025-12-19 17:33:09 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+const root = document.getElementById("root");
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+if (root) {
+  createRoot(root).render(<App />);
+}
