c68eb5daa8ec21705d7580b33a32a4d202988753
vivanova288-boop <vivanova288@gmail.com>
2025-12-19 18:15:16 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: '/Courcefigma/',
+  base: '/Courcefigma/', // ИМЯ РЕПОЗИТОРИЯ, БЕЗ ОШИБОК
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
-    outDir: 'docs'
-  }
+    outDir: 'dist',
+  },
 })
