3556673596e40cf9e5aac8f2a8d6a390af1e3309
vivanova288-boop <vivanova288@gmail.com>
2025-12-19 17:37:09 +0500

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,1 @@
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-
-export default {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-  ],
-}
-
+export default {}
